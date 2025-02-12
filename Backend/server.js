2fc5b937fc7c45e3152fb4c45a1171836960fa58
jk2006k <jk05kishoore@gmail.com>
2025-02-12 19:29:40 +0530

    changes made

--- Backend/server.js
+++ Backend/server.js
@@ -1,32 +1,25 @@
-const express=require('express');
+const express = require('express');
+const connectDB = require('./config/db');
 
-const app=express();
+const app = express();
+const PORT = 3000;
 
+app.use(express.json());
 
-const PORT=3000;
-
-const connectDB=require('./config/db');
-
-app.use(express.json())
-
-app.get('/ping', async (req,res)=>{
-    try{
+app.get('/ping', async (req, res) => {
+    try {
         res.send('Ping is Working Fine...');
-    }catch(error){
-        res.status(500).send('Internal Server error')}
-});
-
-app.listen(PORT, async  ()=>{
-    try{
-        await connectDB()
-        console.log(`Server is running on port ${PORT}`);
-    }catch(error){
-        console.log('Server error',error)
+    } catch (error) {
         res.status(500).send('Internal Server Error');
     }
 });
 
-
-
-
-
+app.listen(PORT, async () => {
+    try {
+        await connectDB();
+        console.log(`Server is running on port ${PORT}`);
+    } catch (error) {
+        console.error('Server error:', error);
+        process.exit(1);
+    }
+});
