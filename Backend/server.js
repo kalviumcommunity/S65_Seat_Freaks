f41bf0d95442056957ffdc4ff2e573ea409643a1
jk2006k <jk05kishoore@gmail.com>
2025-02-12 19:21:59 +0530

    bruno api test

--- Backend/server.js
+++ Backend/server.js
@@ -2,23 +2,31 @@ const express=require('express');
 
 const app=express();
 
+
 const PORT=3000;
 
-app.get('/',(req,res)=>{
+const connectDB=require('./config/db');
+
+app.use(express.json())
+
+app.get('/ping', async (req,res)=>{
     try{
-        res.send('Hello World');
+        res.send('Ping is Working Fine...');
     }catch(error){
         res.status(500).send('Internal Server error')}
 });
 
-app.listen(PORT,()=>{
+app.listen(PORT, async  ()=>{
     try{
+        await connectDB()
         console.log(`Server is running on port ${PORT}`);
     }catch(error){
         console.log('Server error',error)
         res.status(500).send('Internal Server Error');
     }
 });
+
+
 
 
 
